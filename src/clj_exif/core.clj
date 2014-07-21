481753e7f1f87d43bd57adfff8c36c79c0de060a
Oli McCormack <oli@olimcc.com>
2014-07-20 21:39:18 -0700

    Better core.
    - Better naming and docstrings.
    - Clear comment cruft.
    - Close write streams after use.

--- src/clj_exif/core.clj
+++ src/clj_exif/core.clj
@@ -3,9 +3,9 @@
   -> metadata
   ----> directory
   -------> tag (metadata, value)"
+  (:refer-clojure :exclude [read])
   (:require [clojure.java.io :as jio]
-            [clojure.pprint :refer :all]
-            )
+            [clojure.pprint :refer :all])
   (:import [org.apache.commons.imaging Imaging]
            [org.apache.commons.imaging.formats.tiff.write TiffOutputSet]
            [org.apache.commons.imaging.formats.tiff.constants AllTagConstants]
@@ -15,12 +15,10 @@
            [org.apache.commons.imaging.common RationalNumber]
            [java.io FileOutputStream]
            [org.apache.commons.imaging.formats.jpeg.exif ExifRewriter]
-           [java.io BufferedOutputStream]
-           ))
-
+           [java.io BufferedOutputStream]))
 
 (defn get-metadata
-  "Get metadata for a filepath"
+  "Get metadata for a File."
   [^java.io.File file]
   (-> file
       (Imaging/getMetadata)))
@@ -32,12 +30,11 @@
   (-> metadata .getExif (.findDirectory directory-type)))
 
 (defn get-directories
- "Read only directories present in metadata."
+  "Read only directories present in metadata."
   [metadata]
   (let [directories (-> metadata .getExif .getDirectories)]
     (map #(get-directory metadata (.type %)) directories)))
 
-;; either get a read directory or a writeable output set
 (defn get-output-set
   "Writeable metadata directories.
   Returns: TiffOutputSet"
@@ -59,108 +56,45 @@
   (into {} (map (fn [field]
                   [(.getTagName field) (.getValue field)]) fields)))
 
-(defn crude-exif-map
+(defn read
   "Generate a clojure map of directories/data from metadata."
   [metadata]
   (->> metadata
        get-directories
        (map (fn [directory]
-                [(.description directory)
-                 (-> directory get-fields fields->map)]))
+              [(.description directory)
+               (-> directory
+                   get-fields
+                   fields->map)]))
        (into {})))
 
 (defn copy-file-with-new-metadata
   [from-file to-file output-set]
-  (let [exif-rewriter (ExifRewriter.)
-        output-buffer (-> to-file
-                          (FileOutputStream.)
-                          (BufferedOutputStream.))]
-    (.updateExifMetadataLossless exif-rewriter
-                                 from-file
-                                 output-buffer
-                                 output-set)))
-
-(defn update-field
-  [output-dir tag-info value]
-  (let [new-value (into-array (class value) [value])]
-  (doto output-dir
-    (.removeField tag-info)
-    (.add tag-info new-value))))
-
+  (with-open [output-stream (-> to-file
+                                (FileOutputStream.)
+                                (BufferedOutputStream.))]
+    (let [exif-rewriter (ExifRewriter.)]
+      (.updateExifMetadataLossless exif-rewriter
+                                   from-file
+                                   output-stream
+                                   output-set))))
 (defn get-tag-info
   [output-dir tag]
   (-> output-dir
       (.findField tag)
       .tagInfo))
 
+(defn update-value
+  "Update an output set with a new value for a dir+tag combination.
+  Performs an in-place modification."
+  [output-set directory tag values]
+  (let [output-dir (get-output-directory output-set directory)
+        tag-info (get-tag-info output-dir tag)
+        new-value (into-array (-> values first class) values)]
+    (doto output-dir
+      (.removeField tag-info)
+      (.add tag-info new-value))))
+
 (defn get-value
   [metadata tag-info]
   (.findEXIFValueWithExactMatch metadata tag-info))
- 
-(comment
-  ;; read examples
-  (->> "/home/oliver/IMG_20140607_163042357.jpg"
-       get-metadata
-       get-directories
-       (map #(-> % get-fields fields->map)))
-  
-(-> "/home/oliver/IMG_20140607_163042357.jpg"
-    get-metadata
-    (get-directory TiffDirectoryConstants/DIRECTORY_TYPE_ROOT)
-    get-fields
-    fields->map
-    )
-
-(-> "/home/oliver/IMG_20140607_163042357.jpg"
-      get-metadata
-      get-output-set)
-  
-  (-> "/home/oliver/output2.jpg"
-      get-metadata
-      crude-exif-map
-      pprint
-      )
-
-(-> "/home/oliver/IMG_20140607_163042357.jpg"
-      get-metadata
-      crude-exif-map
-      pprint
-      )
-
-  (-> "/home/oliver/IMG_20140607_163042357.jpg"
-      get-metadata
-      (get-directory TiffDirectoryConstants/DIRECTORY_TYPE_ROOT)
-      get-fields
-      fields->map)
-
-(let [f "/home/oliver/IMG_20140607_163042357.jpg"
-      input-file (java.io.File. f)
-      output-file (java.io.File. "/home/oliver/output3.jpg")
-      md (get-metadata input-file)
-      output-set (get-output-set md)
-      output-dir (get-output-directory
-                        output-set
-                        TiffDirectoryConstants/DIRECTORY_TYPE_ROOT)
-      tag-to-modify TiffTagConstants/TIFF_TAG_MAKE
-      tag-info (get-tag-info output-dir tag-to-modify)
-      current-field-value (get-value md tag-info)
-      new-field-value "OLICORP2"]
-
-  (println (.getSeperateValue (.findField output-dir tag-info)))
-
-  #_(update-field output-dir tag-info new-field-value)
-  #_(copy-file-with-new-metadata input-file output-file output-set))
-
-
-  (let [f (java.io.File. "/home/oliver/IMG_20140607_163042357.jpg")
-        md (Imaging/getMetadata f)
-        output-set (.getOutputSet (.getExif md))
-        exif-dir (get-directory output-set TiffDirectoryConstants/DIRECTORY_TYPE_ROOT)
-        key-to-set TiffTagConstants/TIFF_TAG_MAKE
-        field-tag-info (.tagInfo (.findField exif-dir key-to-set))
-        current-field-value (.findEXIFValueWithExactMatch md field-tag-info)]
-    (println (.getStringValue current-field-value)))
-
-  )
-
-
