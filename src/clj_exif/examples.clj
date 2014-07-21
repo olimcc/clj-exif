5cb906443342670d948bdf4182e84436459f7dc2
Oli McCormack <oli@olimcc.com>
2014-07-20 21:41:12 -0700

    Tests, demo usage and swap of example file.

---
+++ src/clj_exif/examples.clj
@@ -0,0 +1,49 @@
+(ns clj-exif.examples
+  "Examples usage of the exif utilities."
+  (:require [clojure.java.io :as jio]
+            [clojure.pprint :as pprint]
+            [clj-exif.core :as exif])
+  (:import
+    [java.io File]
+    [org.apache.commons.imaging.formats.tiff.constants
+     TiffDirectoryConstants
+     TiffTagConstants
+     GpsTagConstants]
+    [org.apache.commons.imaging.common RationalNumber]))
+
+;; read data
+(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+      metadata (exif/get-metadata input-file)]
+  (println (pprint/pprint (exif/read metadata))))
+
+(let [input-file (File. "/tmp/output.jpg")
+      metadata (exif/get-metadata input-file)]
+  (println (pprint/pprint (exif/read metadata))))
+
+;; update the make of the device
+(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+      output-file (File. "/tmp/output.jpg")
+      metadata (exif/get-metadata input-file)
+      ;; output-set is a writeable copy of the data retrieve from input-file.
+      output-set (exif/get-output-set metadata)]
+  (exif/update-value output-set
+                     TiffDirectoryConstants/DIRECTORY_TYPE_ROOT
+                     TiffTagConstants/TIFF_TAG_MAKE
+                     ["this is a new value"])
+  (exif/copy-file-with-new-metadata input-file output-file output-set))
+
+;; update the longitude co-ordinate
+(let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+      output-file (File. "/tmp/output.jpg")
+      metadata (exif/get-metadata input-file)
+      ;; output-set is a writeable copy of the data retrieve from input-file.
+      output-set (exif/get-output-set metadata)]
+  (exif/update-value output-set
+                     TiffDirectoryConstants/DIRECTORY_TYPE_GPS
+                     GpsTagConstants/GPS_TAG_GPS_LONGITUDE
+                     [(RationalNumber. 11 1)
+                      (RationalNumber. 11 1)
+                      (RationalNumber. 32 100)])
+  (exif/copy-file-with-new-metadata input-file output-file output-set))
+
+
--- test/clj_exif/core_test.clj
+++ test/clj_exif/core_test.clj
@@ -1,7 +1,41 @@
 (ns clj-exif.core-test
-  (:require [clojure.test :refer :all]
-            [clj-exif.core :refer :all]))
+  (:require [clj-exif.core :as exif]
+            [clojure.test :refer :all]
+            [clojure.java.io :as jio])
+  (:import [org.apache.commons.imaging.common RationalNumber]
+           [java.io File]
+           [org.apache.commons.imaging.formats.tiff.constants
+            TiffDirectoryConstants
+            TiffTagConstants]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest read-exif-metadata
+  (testing "read exif"
+    (let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+          metadata (exif/get-metadata input-file)
+          data (exif/read metadata)]
+      (is (map? data))
+      (is (= (get-in data ["Root" "Software"]) "Google"))
+      (is (= (class (get-in data ["Root" "XResolution"])) RationalNumber))
+      (let [version (get-in data ["Exif" "ExifVersion"])]
+        (is (= (count version) 4))
+        (is (= (first version) 48)))
+      (let [lng (get-in data ["Gps" "GPSLongitude"])]
+        (is (= (long (first lng)) 121))))))
+
+(deftest write-exif-metadata
+  (testing "write exif"
+    (let [output-file (File/createTempFile "/tmp/output" ".jpg")]
+    (try
+      (let [input-file (jio/as-file (jio/resource "pic.geo.jpg"))
+            metadata (exif/get-metadata input-file)
+            output-set (exif/get-output-set metadata)
+            rand-str (str (rand-int 10000))]
+        (exif/update-value output-set
+                           TiffDirectoryConstants/DIRECTORY_TYPE_ROOT
+                           TiffTagConstants/TIFF_TAG_MAKE
+                           [rand-str])
+        (exif/copy-file-with-new-metadata input-file output-file output-set)
+        (is  (= (-> output-file exif/get-metadata exif/read (get-in ["Root" "Make"]))
+                rand-str)))
+      (finally (.delete output-file))))))
+
